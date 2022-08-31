7b1360d3e79597629520f2a94969267ca83c8241
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-31 09:49:44 +0600

    user set by google signin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import './App.css';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
+import auth from './firebase.init';
+
 
 function App() {
+  const [users, setUsers] = useState({});
+  const googleProvider = new GoogleAuthProvider();
+
+  const googleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        setUsers(user)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+
   return (
     <div className='container'>
       <h2>Firebase Authentication</h2>
       <div className='field'>
-        <button>Google Sing In</button>
+        <button onClick={googleSignIn}>Google Sing In</button>
         <br />
         <br />
         <button>Github Sign In</button>
         <br />
         <br />
         <button>Facebook Sign in</button>
+      </div>
+      <div className=''>
+        <h4>User: {users.displayName}</h4>
+        <h4>Email: {users.email}</h4>
+        <img src={users.photoURL} alt="" />
+
       </div>
 
 
